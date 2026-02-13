d5cd88d196987a73b84ad0899881732ee4316eac
Daniela R <daniramirezg@udistrital.edu.co>
2026-02-12 21:41:28 -0500

    versión 1

---
+++ Backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app= FastAPI()
+# Cors: Mecanismo de seguridad. Se utiliza para habilitar peticiones desde clientes que no estan en mi dominio.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    all_credentials= True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+@app.get("/sumar")
+def sumar_numeros(a:float, b:float):
+    return a+b
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Proyecto demo Básico en Python
+
+## Para el Backend
+1. Crear un ambiente virtual
+2. Cargar biblioteca fastapi (pip install fastapi)
+3. Cargar biblioteca uvicornn
